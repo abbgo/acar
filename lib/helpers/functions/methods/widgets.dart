746d026521eec03a54ab54277fb78b6207031cb0
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-05-29 14:08:39 +0500

    feat: add errorMethod and loadMethod

---
+++ lib/helpers/functions/methods/widgets.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Center errorMethod(Object error) => Center(child: Text(error.toString()));
+Center loadMethod() => const Center(child: CircularProgressIndicator());
--- lib/pages/dates/dates.dart
+++ lib/pages/dates/dates.dart
@@ -1,10 +1,22 @@
+import 'package:acar/helpers/functions/methods/widgets.dart';
+import 'package:acar/models/dates.dart';
+import 'package:acar/providers/models/dates.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class DatesPage extends StatelessWidget {
+class DatesPage extends ConsumerWidget {
   const DatesPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text('Dates page'));
+  Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue<ResultDates> files = ref.watch(fetchFilesProvider);
+
+    return files.when(
+      data: (data) {
+        return const Center(child: Text('Dates page'));
+      },
+      error: (error, stackTrace) => errorMethod(error),
+      loading: () => loadMethod(),
+    );
   }
 }
