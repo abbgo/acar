1b13989e6f071a449ea7fef7437b153569804e85
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-05-29 12:56:12 +0500

    feat: add fetchFilesProvider provuider

--- lib/providers/models/dates.dart
+++ lib/providers/models/dates.dart
@@ -1,4 +1,24 @@
+import 'package:acar/models/dates.dart';
+import 'package:acar/providers/local_storadge.dart';
 import 'package:acar/services/dates.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final dateApiProvider = Provider<DatesApiService>((ref) => DatesApiService());
+
+var fetchFilesProvider = FutureProvider<ResultDates>(
+  (ref) async {
+    ResultDates result = ResultDates.defaultResult();
+    String token = ref.read(tokenProvider);
+
+    print('------------------------------------------------------------------');
+    print(token);
+
+    try {
+      List<Dates> files = await ref.read(dateApiProvider).fetchFiles(token);
+      result = ResultDates(error: '', dates: files);
+    } catch (e) {
+      result = ResultDates(error: e.toString());
+    }
+    return result;
+  },
+);
