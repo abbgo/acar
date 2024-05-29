0f7e22cc937a468a865462e592631c42f1d3fe83
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-05-29 14:57:10 +0500

    feat: add fetchHostsByFileNameProvider provider

--- lib/providers/models/host.dart
+++ lib/providers/models/host.dart
@@ -1,4 +1,30 @@
+import 'package:acar/models/host.dart';
+import 'package:acar/providers/internet.dart';
+import 'package:acar/providers/local_storadge.dart';
 import 'package:acar/services/host.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final hostsApiProvider = Provider<HostApiService>((ref) => HostApiService());
+
+var fetchHostsByFileNameProvider =
+    FutureProvider.family<ResultHosts, HostParams>(
+  (ref, params) async {
+    ResultHosts result = ResultHosts.defaultResult();
+    String token = ref.read(tokenProvider);
+    bool hasInternet =
+        await ref.read(checkInternetConnProvider(params.context!).future);
+
+    if (hasInternet) {
+      try {
+        List<Host> hosts = await ref
+            .read(hostsApiProvider)
+            .fetchHostsByFileName(token, params.filename);
+        result = ResultHosts(error: '', hosts: hosts);
+      } catch (e) {
+        result = ResultHosts(error: e.toString());
+      }
+    }
+
+    return result;
+  },
+);
--- lib/services/host.dart
+++ lib/services/host.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:acar/helpers/static_data.dart';
 import 'package:acar/models/host.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class HostApiService {
@@ -23,4 +25,18 @@ class HostApiService {
       rethrow;
     }
   }
+}
+
+class HostParams extends Equatable {
+  final BuildContext? context;
+  final String filename;
+
+  const HostParams({required this.context, required this.filename});
+
+  factory HostParams.defaultResult() {
+    return const HostParams(context: null, filename: '');
+  }
+
+  @override
+  List<Object?> get props => [context, filename];
 }
