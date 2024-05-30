16782e801ee1a727d41489f9149de2c0e143edbe
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-05-30 16:12:57 +0500

    fix: update hosts page

--- lib/pages/hosts/hosts.dart
+++ lib/pages/hosts/hosts.dart
@@ -3,12 +3,17 @@ import 'package:acar/models/host.dart';
 import 'package:acar/providers/models/host.dart';
 import 'package:acar/services/host.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HostsPage extends StatelessWidget {
   const HostsPage({super.key, required this.filename});
 
   final String filename;
+
+  Future<void> copyToClipboard(String text) async {
+    Clipboard.setData(ClipboardData(text: text));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,33 +25,61 @@ class HostsPage extends StatelessWidget {
         scrolledUnderElevation: 0,
         backgroundColor: Colors.white,
       ),
-      body: Consumer(
-        builder: (context, ref, child) {
-          HostParams params = HostParams(context: context, filename: filename);
-          var hosts = ref.watch(fetchHostsByFileNameProvider(params));
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Consumer(
+          builder: (context, ref, child) {
+            HostParams params =
+                HostParams(context: context, filename: filename);
+            var hosts = ref.watch(fetchHostsByFileNameProvider(params));
 
-          return hosts.when(
-            data: (data) {
-              if (data.error != '' || data.hosts == null) {
-                return const Center(child: Text('Yalnyslyk yuze cykdy'));
-              }
+            return hosts.when(
+              data: (data) {
+                if (data.error != '' || data.hosts == null) {
+                  return const Center(child: Text('Yalnyslyk yuze cykdy'));
+                }
 
-              List<Host> hosts = data.hosts!;
+                List<Host> hosts = data.hosts!;
 
-              return ListView.builder(
-                physics: const BouncingScrollPhysics(),
-                itemCount: hosts.length,
-                itemBuilder: (context, index) {
-                  Host host = hosts[index];
-                  return Text(host.hostname);
-                },
-              );
-            },
-            error: (error, stackTrace) => errorMethod(error),
-            loading: () =>
-                const Center(child: Text('Garasmagynyzy hayys edyaris ...')),
-          );
-        },
+                return ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: hosts.length,
+                  itemBuilder: (context, index) {
+                    Host host = hosts[index];
+                    return Card(
+                      color: Colors.blueAccent,
+                      elevation: 3,
+                      child: ListTile(
+                        title: Text(host.location.country),
+                        subtitle: Text('${host.ip}:${host.port}'),
+                        trailing: IconButton(
+                          onPressed: () async {
+                            await copyToClipboard(host.toString());
+                            if (context.mounted) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Maglumatlar kopyalandy !'),
+                                ),
+                              );
+                            }
+                          },
+                          icon: const Icon(Icons.copy, color: Colors.white),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+              error: (error, stackTrace) => errorMethod(error),
+              loading: () => const Center(
+                child: Text(
+                  'Garasmagynyzy hayys edyaris ...',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
