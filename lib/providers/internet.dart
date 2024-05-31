fafa201ad94d60ab001aab2b95f64fdd6355d6a9
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-05-31 12:19:16 +0500

    fix: update checkInternetConnProvider

--- lib/providers/internet.dart
+++ lib/providers/internet.dart
@@ -1,8 +1,13 @@
 import 'package:acar/helpers/functions/internet.dart';
+import 'package:acar/providers/pages/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 var checkInternetConnProvider =
     FutureProvider.autoDispose.family<bool, BuildContext>((ref, arg) async {
-  return await checkIntConn(arg);
+  bool hasInternet = await checkIntConn(arg);
+  if (!hasInternet) {
+    ref.read(loadLoginProvider.notifier).state = false;
+  }
+  return hasInternet;
 });
