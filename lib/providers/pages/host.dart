71752bb879d2bca01fc8825566c80913082eab21
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-06-01 10:03:23 +0500

    feat: add resultHostsProvider provider and add ResultHostsProvider

---
+++ lib/providers/pages/host.dart
@@ -0,0 +1,15 @@
+import 'package:acar/models/host.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ResultHostsProvider extends StateNotifier<List<Host>> {
+  ResultHostsProvider() : super([]);
+
+  Future<void> addHost(Host host) async {
+    state = [...state, host];
+  }
+}
+
+final resultHostsProvider =
+    StateNotifierProvider.autoDispose<ResultHostsProvider, List<Host>>(
+  (ref) => ResultHostsProvider(),
+);
